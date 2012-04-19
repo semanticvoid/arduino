789f306d0042989b25dbc315f2bc79861a1ad813
semanticvoid <anand@semanticvoid.com>
2012-04-18 21:16:52 -0700

    added LCD display

--- watr/watr.ino
+++ watr/watr.ino
@@ -3,22 +3,33 @@
 // #################
 
 #include <EEPROM.h>
+#include <LiquidCrystal.h>
 
-#define PIN 8
-#define VOLINCR 0.1
+#define PIN 10
+#define VOLINCR 1
+#define FLASHINTERVAL 100
 
 union {
   int i;
   float f;
 } vol;
 
+// lcd
+LiquidCrystal lcd(12,13,0,1,2,3,4,5,6,7,8);
+
 void setup() {
-  Serial.begin(9600);
+  lcd.begin(16,2);
+  
+  // display title
+  lcd.setCursor(2,0);
+  lcd.print("Project Watr");
+  
+  //Serial.begin(9600);
   pinMode(PIN, INPUT);
   digitalWrite(PIN, LOW);
   fetchVol();
-  Serial.print("reading saved vol ... ");
-  Serial.print(vol.f);
+  //Serial.print("reading saved vol ... ");
+  //Serial.print(vol.f);
 }
 
 void loop() {
@@ -26,9 +37,24 @@ void loop() {
   if(state == 1) {
     incr();
     //storeVol();
-    Serial.println(vol.f);
+    lcd.setCursor(4,1);
+    lcd.print(vol.f);
+    lcd.print(" mL");
   }
-  delay(1000);
+  // flash
+    if(((int)vol.f) % FLASHINTERVAL == 0) {
+      for(int i=0; i<2; i++) {
+        lcd.noDisplay();
+        digitalWrite(PIN, HIGH);
+        delay(200);
+        lcd.display();
+        digitalWrite(PIN, LOW);
+        delay(200);
+      }
+      delay(200);
+    } else {
+      delay(1000);
+    }
 }
 
 // function to read vol from EEPROM
@@ -49,4 +75,3 @@ void storeVol() {
 void incr() {
   vol.f = fetchVol() + VOLINCR;
 }
-
